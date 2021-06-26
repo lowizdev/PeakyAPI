d7630a94a1d9c4b14ddf2d5c8f0768f9883bd396
Luiz Antonio Santos Gonsalves <santosgonsalvesjr@gmail.com>
2021-06-25 23:13:37 -0300

    First Commit

---
+++ Peaky/Controllers/HorsesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Peaky.Infra.PgSQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peaky.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HorsesController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get() {
+
+            HorseRepository hr1 = new HorseRepository();
+
+            hr1.TestQuery();
+
+            return Ok("Hello");
+
+        }
+
+    }
+}
---
+++ Peaky/Infra/PgSQL/HorseRepository.cs
@@ -0,0 +1,33 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peaky.Infra.PgSQL
+{
+    public class HorseRepository
+    {
+
+        public async void TestQuery() {
+
+            var connString = "";
+
+            await using var conn = new NpgsqlConnection(connString);
+
+            await conn.OpenAsync();
+
+            await using (var cmd = new NpgsqlCommand("SELECT * FROM horse", conn)) {
+
+                var res = await cmd.ExecuteReaderAsync();
+                res.Read();
+                res.Read();
+                var aaa = res["name"];
+                Console.WriteLine(res);
+            
+            }
+
+        }
+
+    }
+}
